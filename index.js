37391ca553a86a1fed91b8934a424870123db0e8
andrewErgo <andrew@ergo.agency>
2023-03-16 23:38:10 +0000

    more command

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const { Command } = require('commander');
+const {
+    Command
+} = require('commander');
 const program = new Command();
 
-const { exec } = require('node:child_process');
+const {
+    exec
+} = require('node:child_process');
 
 console.log("Hello Mom")
 
-exec('bcdedit /enum firmware', (error, stdout, stderr) => { console.log(stdout); });
+const ubuntuPath = String.raw`bcdedit /set "{bootmgr}" path \EFI\ubuntu\grubx64.efi`
+console.log(ubuntuPath)
+const windowPath = String.raw`bcdedit /set "{bootmgr}" path \EFI\Microsoft\Boot\bootmgfw.efi`
+//    exec(ubuntuPath, (error, stdout, stderr) => {
+//        //console.log(error);
+//        console.log(stdout);
+//    })
+
+   exec(windowPath,(error, stdout, stderr)=>{
+       console.log(stdout);
+   })
+
+
+exec('bcdedit /enum firmware', (error, stdout, stderr) => {
+    console.log(stdout);
+});
+
+
+//exec('shutdown /r -t 15')
